db0e46ac2279a807689bb0ce69808a1567c91834
averoes12 <hz.ir.daff@gmail.com>
2020-05-10 09:50:08 +0700

    Do challenge Booleans and Comparison Operators

---
+++ Challenge/Programming in Kotlin/Booleans and Comparison Operators.kt
@@ -0,0 +1,16 @@
+fun main(){
+//    Challenge 1
+
+    val myPassword = "09876543sjfjsaf"
+
+    val isSecured = myPassword.length >= 10
+    println("Password is Secure : $isSecured")
+
+//    Challenge 2
+
+    val person1 = "Jhon Doe"
+    val person2 = "James Andrew"
+
+    val isSameLength = person1.length == person2.length
+    print("Is the name same length ? $isSameLength")
+}
