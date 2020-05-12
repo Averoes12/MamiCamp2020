baa228df44ba4fca025c287a2ac579dbf029d96e
averoes12 <hz.ir.daff@gmail.com>
2020-05-12 15:08:19 +0700

    add new project from class Beginner Android Layouts

--- TimeFighter/app/src/main/java/com/averoes/timefighter/MainActivity.kt
+++ TimeFighter/app/src/main/java/com/averoes/timefighter/MainActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -52,6 +55,29 @@ class MainActivity : AppCompatActivity() {
             view.startAnimation(bounceAnimator)
             incrementScore()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.info, menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.about){
+            showInfo()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showInfo() {
+        val dialogTitle = getString(R.string.aboutTitle, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.aboutMessage)
+
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setTitle(dialogTitle)
+        dialogBuilder.setMessage(dialogMessage)
+        dialogBuilder.create().show()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
